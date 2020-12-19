3e385dbd64c268795f06bbdef9e79bef37b9a3ac
jeongjae <rchmas@gmail.com>
2020-12-19 17:45:54 +0900

    b

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ sequelize.sync({ alter: true })
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
